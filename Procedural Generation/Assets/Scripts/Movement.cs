eb13dcc5da649c07ff372dc45a7dead26db73079
chiwayne <chiwayne@usc.edu>
2016-02-22 03:37:56 -0800

    Procedural Generation complete for now

--- Procedural Generation/Assets/Scripts/Movement.cs
+++ Procedural Generation/Assets/Scripts/Movement.cs
@@ -18,9 +18,18 @@ public class Movement : MonoBehaviour {
 			Vector2 v = rigidBody.velocity;
 			v.y = -5;
 			rigidBody.velocity = v;
+		} else if (Input.GetKey ("left")) {
+			Vector2 v = rigidBody.velocity;
+			v.x = -5;
+			rigidBody.velocity = v;
+		} else if (Input.GetKey ("right")) {
+			Vector2 v = rigidBody.velocity;
+			v.x = 5;
+			rigidBody.velocity = v;
 		} else {
 			Vector2 v = rigidBody.velocity;
 			v.y = 0;
+			v.x = 0;
 			rigidBody.velocity = v;
 		}
 	}
