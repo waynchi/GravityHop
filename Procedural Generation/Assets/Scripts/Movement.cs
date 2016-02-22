d2395e8ec57e62792d9da12b073e152740e871bd
chiwayne <chiwayne@usc.edu>
2016-02-22 01:02:21 -0800

    Procedural Generation Trasnfer

---
+++ Procedural Generation/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	// Use this for initialization
+	private Rigidbody2D rigidBody;
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			Vector2 v = rigidBody.velocity;
+			v.y = 5;
+			rigidBody.velocity = v;
+		} else if (Input.GetKey ("down")) {
+			Vector2 v = rigidBody.velocity;
+			v.y = -5;
+			rigidBody.velocity = v;
+		} else {
+			Vector2 v = rigidBody.velocity;
+			v.y = 0;
+			rigidBody.velocity = v;
+		}
+	}
+}
