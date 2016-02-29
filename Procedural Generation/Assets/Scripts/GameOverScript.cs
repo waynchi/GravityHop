7940a32516e17cdcf7d51a1deae76038b4722167
Wayne Chi <waynchi@gmail.com>
2016-02-28 23:44:37 -0800

    Game over part 2

--- Procedural Generation/Assets/Scripts/GameOverScript.cs
+++ Procedural Generation/Assets/Scripts/GameOverScript.cs
@@ -4,32 +4,20 @@ using System;
 
 public class GameOverScript : MonoBehaviour {
 
-	public float restartDelay = 1000f;
-
 	Animator anim;
-	float restartTimer;
-	bool restart;
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-		restart = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		restart = false;
 		if (Input.GetKey ("g")) {
-			while (!restart) {
-				anim.SetTrigger ("GameOver");
-
-				restartTimer += Time.deltaTime;
-
-				if (restartTimer >= restartDelay) {
-					Console.Out.WriteLine ("Test");
-					UnityEngine.SceneManagement.SceneManager.LoadScene (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex);
-					//Application.LoadLevel (Application.loadedLevel);
-				}
-			}
+			anim.SetTrigger ("GameOver");
+		}
+		if (Input.GetKey ("r")) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex);
 		}
 	}
 }
