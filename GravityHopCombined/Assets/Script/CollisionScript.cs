8b8f1e15f1c47157828deab9e8df3c31be316d5e
chiwayne <chiwayne@usc.edu>
2016-02-29 17:23:59 -0800

    Branch commit. End of class.

--- GravityHopCombined/Assets/Script/CollisionScript.cs
+++ GravityHopCombined/Assets/Script/CollisionScript.cs
@@ -3,15 +3,27 @@ using System.Collections;
 
 public class CollisionScript : MonoBehaviour {
 
+	public Canvas HUDCanvas;
+	public Canvas ScoreCanvas;
+	Animator anim;
+	ScoreSystem scoreScript;
     ScoreSystem ss;
     bool invincibility;
-    public Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
+
+	public float restartDelay;
+
+	private float restartCount;
     
     // Use this for initialization
     void Start () {
         ss = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreSystem>();
         rb2d = GetComponent<Rigidbody2D>();
         invincibility = false;
+		anim = HUDCanvas.GetComponent<Animator> ();
+		scoreScript = ScoreCanvas.GetComponentInChildren<ScoreSystem> ();
+		restartDelay = 5f;
+		restartCount = 0;
     }
 
     void OnCollisionEnter2D(Collision2D col) {
@@ -30,48 +42,50 @@ public class CollisionScript : MonoBehaviour {
                 float angle = Vector3.Angle(ortho, rb2d.velocity);
 
                 // if the ship takes a direct hit
-                if (angle <= 30)
-                    Destroy(gameObject);
+				if (angle <= 30) {
+					Destroy (gameObject);
+					GameOver ();
+				}
             }
         }
         else if (name.Contains("Drone")) // Floating drone that will patrol back and forth
         {
-            if (invincibility)
-            {
-                Destroy(col.collider.gameObject);
-                invincibility = false;
-            }
-            else Destroy(gameObject);
-            // GAME OVER
+			if (invincibility) {
+				Destroy (col.collider.gameObject);
+				invincibility = false;
+			} else {
+				Destroy (gameObject);
+				GameOver (); 
+			}
         }
 
         else if (name.Contains("Hole")) // Instant game over
         {
             Destroy(gameObject);
-            // GAME OVER
+			GameOver ();
         }
 
         else if (name.Contains("Laser")) // Laser fields that can kill player: turns on and off
         {
-            if (invincibility)
-            {
-                Destroy(col.collider.gameObject);
-                invincibility = false;
-            }
-            else Destroy(gameObject);
-            // GAME OVER
+			if (invincibility) {
+				Destroy (col.collider.gameObject);
+				invincibility = false;
+			} else {
+				Destroy (gameObject);
+				GameOver ();
+			}
         }
 
         else if (name.Contains("Wall")) // Circular wall that rotates around a point
         {
             Destroy(gameObject);
-            // GAME OVER
+			GameOver ();
         }
 
         else if (name.Contains("Enemy")) // Enemy chases the player on the planet
         {
             Destroy(gameObject);
-            // GAME OVER
+			GameOver ();
         }
 
         else if (name.Contains("CoinSprite")) // adds on to the score
@@ -107,8 +121,21 @@ public class CollisionScript : MonoBehaviour {
 
     }
 
+	void GameOver () {
+		anim.SetTrigger ("GameOver");
+		scoreScript.stop = true;
+		HUDCanvas.GetComponentInChildren<UnityEngine.UI.Button> ().enabled = true;
+	}
+
     // Update is called once per frame
     void Update () {
-	    
+		if (scoreScript.stop == true) {
+			if (restartCount >= restartDelay) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex); 
+				restartCount = 0;
+				scoreScript.stop = false;
+			}
+			restartCount += Time.deltaTime;
+		}
 	}
 }
