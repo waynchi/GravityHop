fb98a95e97a52b52a69036116c30f8b09e9046b9
Wayne Chi <waynchi@gmail.com>
2016-03-06 23:50:03 -0800

    Bugged. Handing off to xuechao

--- GravityHopCombined/Assets/Script/Planet.cs
+++ GravityHopCombined/Assets/Script/Planet.cs
@@ -4,19 +4,27 @@ using System.Collections;
 public class Planet : MonoBehaviour {
 	private Rigidbody2D rg2d;
 	private PointEffector2D ef2d;
+	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		rg2d = GetComponent<Rigidbody2D> ();
 		ef2d = GetComponent<PointEffector2D> ();
 		rg2d.mass = 100;
-		ef2d.forceMagnitude = -rg2d.mass;
-		rg2d.freezeRotation = true;
+		ef2d.forceMagnitude = -rg2d.mass*20;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rg2d.angularVelocity = 0;
+		
 	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		coll.gameObject.SendMessage ("Combo");
+	}
+
+
+
 }
--- GravityHopCombined/Assets/Script/PlayerController.cs
+++ GravityHopCombined/Assets/Script/PlayerController.cs
@@ -1,42 +1,127 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed=1;
-	public float NewTonPara;
-	public bool needAntiV = true;
-	public ArrayList MassList = new ArrayList();
-	public Vector2 MassCenter;
+	public float fuel = 100;
+	public Vector2 MassCenter,WalkingDirection,BurstDirecton,LastMassCenter;
 	private Rigidbody2D rg2d;
+	private bool UFOEnableTouch = false,started = false, touchButton = true;
+
+	public bool ComboInitiated = false;
+	public float lastComboTime = 0.0F;
+	public float ComboCount = 0,comboTime = 3;
+	public string displayComboContent; 
+
+	public Text DisplayCombo;
+
 
 
 	void Start()
 	{
+
 		rg2d = GetComponent<Rigidbody2D> ();
 		rg2d.mass = 2;
-		rg2d.AddForce (-2*rg2d.position, ForceMode2D.Impulse);
-		rg2d.freezeRotation = true;
+		rg2d.AddForce (5*rg2d.position, ForceMode2D.Impulse); //Initialize Movement
+
+	}
+
+	void FixedUpdate(){
+		BurstDirecton = (rg2d.position - MassCenter).normalized;
+		WalkingDirection = new Vector2 (-BurstDirecton.y, BurstDirecton.x);
 	}
 
 	void Update()
 	{
-		DetectInput ();
+		if(started)
+			DetectInput ();
 	/*	Vector2 UniversalGravitation = DetectForceObject ();
 		rg2d.AddForce (UniversalGravitation*speed);
-
 	*/
-
+		DisplayCombo.text = displayComboContent;
+			
 	}
 
 
 	void DetectInput (){
+
 		foreach (Touch touch in Input.touches) {
-			if (touch.phase == TouchPhase.Moved) {
-				rg2d.AddForce (touch.deltaPosition*5, ForceMode2D.Force);
+			if (touch.phase == TouchPhase.Ended){
+
+				if (touchButton) {
+					touchButton = false;
+					continue;
+				}
+				if (fuel > 0 && UFOEnableTouch) {
+					rg2d.AddForce (BurstDirecton * 1900, ForceMode2D.Force);
+				}
+				//if(fuel>0)
+				//fuel -= touch.deltaPosition.magnitude/10;
+				UFOEnableTouch = false;
 			}
 		}
+		if (rg2d.velocity.magnitude > 50)
+			rg2d.velocity = rg2d.velocity.normalized * 10;
+		if (rg2d.velocity.magnitude < 10)
+			rg2d.velocity = rg2d.velocity.normalized * 10;
+		if (Time.time - lastComboTime > comboTime) {
+			ComboInitiated = false;
+			ComboCount = 0;
+			displayComboContent = "";
+		}
+
+	}
+		
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		Rigidbody2D coll_rg = coll.gameObject.GetComponent<Rigidbody2D> ();
+		LastMassCenter = MassCenter;
+		MassCenter = coll_rg.position;
+		UFOEnableTouch = true;
 	}
 
+	void GameStart(){
+		started = true;
+	}
+
+	void Combo(){
+		if (started) {
+
+			if (ComboInitiated) {
+				if (Time.time - lastComboTime > comboTime) {
+					ComboInitiated = false;
+					ComboCount = 0;
+					displayComboContent = "";
+				} else {
+					if (!MassCenter.Equals (LastMassCenter)) {
+						ComboCount++;
+						lastComboTime = Time.time;
+						displayComboContent = "Combo: " + ComboCount.ToString () + "  !!!";
+					}
+				}
+
+			} else {
+				if (!MassCenter.Equals (LastMassCenter)) {
+					ComboInitiated = true;
+					ComboCount = 1;
+					lastComboTime = Time.time;
+					displayComboContent = "Combo: " + ComboCount.ToString () + "  !!!";
+				}
+			}
+		}
+
+	}
+/*
+
+	void Landed () {
+		isLanded = true;
+		ForceGiven = false;
+		if(!ForceGiven)rg2d.velocity = rg2d.velocity * 0;
+	}
+
+*/
 }
