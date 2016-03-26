a7478da7ae6c35df0aa03d2c06f4520a8b4078ea
wangal <wangal@usc.edu>
2016-03-25 20:02:48 -0700

    combined start, game over, and restart as HUD; collision done

---
+++ Level-Based Version/GravityHopCombined/Assets/Alex Scripts Prefab/NewGameOverScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class NewGameOverScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("g")) {
+            GameOver();
+		}
+		if (Input.GetKey ("r")) {
+            Restart();
+        }
+	}
+
+    public void GameOver()
+    {
+        anim.SetTrigger("GameOver");
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
