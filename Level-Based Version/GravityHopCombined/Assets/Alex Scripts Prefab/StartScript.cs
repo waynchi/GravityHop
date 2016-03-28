5e18f77696c3278fdd8648e32d5641545b6ca458
wangal <wangal@usc.edu>
2016-03-27 20:35:00 -0700

    created separate start scene

---
+++ Level-Based Version/GravityHopCombined/Assets/Alex Scripts Prefab/StartScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void StartGame()
+    {
+        // BuildIndex
+        // 0: Start Screen
+        // 1: level1
+        SceneManager.LoadScene(1);
+    }
+}
