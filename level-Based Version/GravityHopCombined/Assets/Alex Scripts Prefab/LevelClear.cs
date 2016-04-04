3a7e75f47a50f4f033f9d7ede0b994a17597902c
wangal <wangal@usc.edu>
2016-04-04 16:28:24 -0700

    added level complete via HUD

---
+++ level-Based Version/GravityHopCombined/Assets/Alex Scripts Prefab/LevelClear.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelClear : MonoBehaviour {
+    //central state machine
+    public CentralStateScript stateMachine;
+    Animator anim;
+    bool displayScore;
+    // Use this for initialization
+    void Start () {
+        if (stateMachine == null)
+            stateMachine = GameObject.FindGameObjectWithTag("SM").GetComponent<CentralStateScript>();
+        anim = GetComponent<Animator>();
+        displayScore = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (displayScore == false && stateMachine.getGameState() == CentralStateScript.GameState.Victory)
+        {
+            LevelCleared();
+            displayScore = true;
+        }
+	}
+
+    public void LevelCleared()
+    {
+        Debug.Log("HELLO");
+        anim.SetTrigger("LevelCleared");
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
