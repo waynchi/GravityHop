0284d4fc41b414a7265ba31917d566027b51f606
chiwayne <chiwayne@usc.edu>
2016-04-09 22:41:49 -0700

    Stars working properly

--- Level-Based Version/GravityHopCombined/Assets/Alex Scripts Prefab/StartScript.cs
+++ Level-Based Version/GravityHopCombined/Assets/Alex Scripts Prefab/StartScript.cs
@@ -4,16 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour {
 
+	int numOfLevels = 4;
+
 	// Use this for initialization
 	void Start () {
-	
+		PlayerPrefs.DeleteAll ();
+		for (int i = 0; i < numOfLevels; i++) {
+			if (!PlayerPrefs.HasKey ("Level"+(i+1).ToString())) {
+				PlayerPrefs.SetInt ("Level" + (i+1).ToString(), 2);
+			}
+		}
+
+
 	}
 
-    public void StartGame()
-    {
-        // BuildIndex
-        // 0: Start Screen
-        // 1: level1
-        SceneManager.LoadScene(1);
-    }
+	public void LoadLevel(int level)
+	{
+		// BuildIndex
+		// 0: Start Screen
+		// 1: level1
+		SceneManager.LoadScene(level);
+	}
 }
